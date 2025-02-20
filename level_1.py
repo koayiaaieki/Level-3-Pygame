f727b0b8d5f1bff6df4454fc3d5930c88f2fc9d1
qoyakinator <123508377+koayiaaieki@users.noreply.github.com>
2025-02-20 11:50:01 -0800

    Add files via upload

---
+++ level_1.py
@@ -0,0 +1,70 @@
+import pygame
+from tile_files.tile_creator import Tile_creator
+from utilities.config import *
+#char
+from character_files.character_creator import Character_creator
+#obs
+from obstacle_files.obstacle_creator import Obstacles
+
+class Level_1:
+  def __init__(self):
+    self.screen = pygame.display.get_surface()
+    self.background_group = pygame.sprite.Group()
+    self.obstacle_group = pygame.sprite.Group()
+
+    self.visible_group = pygame.sprite.Group()
+    self.player_group = pygame.sprite.Group()
+
+    self.tile_creator = Tile_creator()
+    self.obstacle_creator = Obstacles()
+    self.character_creator = Character_creator()
+
+    self.create_map()
+
+    self.create_obstacles()
+
+    self.spawn_sprites()
+
+#level uno map
+  def create_map(self):
+    for row_index, row in enumerate(LEVEL_1_MAP):
+      for col_index, tile in enumerate(row):
+        x = col_index * TILE_SIZE
+        y = row_index * TILE_SIZE
+        match tile:
+          case " ":
+            self.tile_creator.tile("grass", (x,y), [self.background_group])
+          case "S":
+            self.tile_creator.tile("sand", (x,y), [self.background_group])
+          case "W":
+            self.tile_creator.tile("water", (x,y), [self.background_group])
+
+#feet
+  def create_obstacles(self):
+    for row_index, row in enumerate(LEVEL_1_OBSTACLES):
+      for col_index, tile in enumerate(row):
+        x = col_index * TILE_SIZE
+        y = row_index * TILE_SIZE
+        match tile:
+          case "X":
+            self.obstacle_creator.tile("bush", (x,y), [self.obstacle_group])
+          case "O":
+            self.obstacle_creator.tile("rock", (x,y), [self.obstacle_group])
+
+  def spawn_sprites(self):
+    for row_index, row in enumerate(LEVEL_1_SPRITES):
+      for col_index, col in enumerate(row):
+        x = col_index * TILE_SIZE
+        y = row_index * TILE_SIZE
+        if col == "C":
+          self.player = self.character_creator.char_basic((x,y), [self.player_group], self.obstacle_group)
+
+  def update(self):
+    #map
+    self.background_group.draw(self.screen)
+    #objacteesles
+    self.obstacle_group.draw(self.screen)
+    self.visible_group.draw(self.screen)
+    #ppayer
+    self.player_group.draw(self.screen)
+    self.player_group.update()
