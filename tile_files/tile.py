3933ef92b121591967ee06a1c69de56b97bdc407
qoyakinator <123508377+koayiaaieki@users.noreply.github.com>
2025-02-21 10:16:47 -0800

    Add files via upload

---
+++ tile_files/tile.py
@@ -0,0 +1,12 @@
+import pygame
+from utilities.config import *
+
+class Tile(pygame.sprite.Sprite):
+  def __init__(self, image, position, groups):
+    super().__init__(groups)
+
+    self.image = image
+
+    self.image = pygame.transform.scale(self.image, (TILE_SIZE, TILE_SIZE))
+
+    self.rect = self.image.get_rect(topleft=position)
